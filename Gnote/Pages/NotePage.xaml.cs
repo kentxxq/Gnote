58634d7ca6e1de5eb50d02962985a887658ba1bd
kentxxq <admin@kentxxq.com>
2022-01-28 21:18:59 +0800

    tmp: 迁移代码

--- Gnote/Pages/NotePage.xaml.cs
+++ Gnote/Pages/NotePage.xaml.cs
@@ -12,6 +12,9 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Gnote.ViewModels;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,9 +26,82 @@ namespace Gnote.Pages
     /// </summary>
     public sealed partial class NotePage : Page
     {
+        private NotePageModel _notePageModel = new();
         public NotePage()
         {
+            this.DataContext = _notePageModel;
             this.InitializeComponent();
+        }
+    }
+
+    class ExplorerItemTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate FolderTemplate { get; set; }
+        public DataTemplate FileTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item)
+        {
+            var explorerItem = (ExplorerItem)item;
+            return explorerItem.Type == ExplorerItem.ExplorerItemType.Folder ? FolderTemplate : FileTemplate;
+        }
+    }
+
+    public class ExplorerItem : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public enum ExplorerItemType { Folder, File };
+        public string Name { get; set; }
+        public ExplorerItemType Type { get; set; }
+        private ObservableCollection<ExplorerItem> m_children;
+        public ObservableCollection<ExplorerItem> Children
+        {
+            get
+            {
+                if (m_children == null)
+                {
+                    m_children = new ObservableCollection<ExplorerItem>();
+                }
+                return m_children;
+            }
+            set
+            {
+                m_children = value;
+            }
+        }
+
+        private bool m_isExpanded;
+        public bool IsExpanded
+        {
+            get { return m_isExpanded; }
+            set
+            {
+                if (m_isExpanded != value)
+                {
+                    m_isExpanded = value;
+                    NotifyPropertyChanged("IsExpanded");
+                }
+            }
+        }
+
+        private bool m_isSelected;
+        public bool IsSelected
+        {
+            get { return m_isSelected; }
+
+            set
+            {
+                if (m_isSelected != value)
+                {
+                    m_isSelected = value;
+                    NotifyPropertyChanged("IsSelected");
+                }
+            }
+
+        }
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
---
+++ Gnote/ViewModels/NotePageModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using Gnote.Interfaces;
+using Windows.Data.Text;
+
+namespace Gnote.ViewModels
+{
+    public class NotePageModel : INotifyPropertyChanged
+    {
+        private string notePath;
+        private int _selectedTreeView;
+        
+        public NotePageModel()
+        {
+            var store = Ioc.Default.GetRequiredService<IStore>();
+            notePath =store.GetNotePath();
+        }
+
+        internal bool NoteTreeViewIsExpanded
+        {
+            get => _selectedTreeView == 0;
+        }
+
+        internal bool LabelTreeViewIsExpanded
+        {
+            get => !NoteTreeViewIsExpanded;
+        }
+
+        internal bool LabelTreeViewIsButtom
+        {
+            get => NoteTreeViewIsExpanded;
+        }
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName=null!)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
